b314cfcf3b9a3452d581e1f019738053ad5f6cfd
Sam Gharcheh <sam.gharacheh@outlook.com>
2025-07-27 16:03:12 +0800

    Sets up basic container application layout
    Initializes the container application with a basic layout,
    including a header and a marketing application component.
    
    Wraps the application with BrowserRouter for routing and
    StylesProvider for consistent styling across microfrontends,
    also adds a custom class name generator to avoid CSS
    conflicts.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+    StylesProvider,
+    createGenerateClassName,
+} from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 
 export default () => {
-  return (
-    <div>
-      <h1>Hi there, here is my other changes!!!!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
+        </BrowserRouter>
+    );
 };
